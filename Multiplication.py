52c437287ae700a6793219e9426d11f7e379edbc
Jbuhrer <152345088+Jbuhrer@users.noreply.github.com>
2024-09-17 09:50:19 -0500

    Commit Multiplication Program

---
+++ Multiplication.py
@@ -0,0 +1,11 @@
+def multiply_numbers(a, b):
+    return a * b
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+
+product = multiply_numbers(num1, num2)
+
+
+print("The product of {0} and {1} is {2}".format(num1, num2, product))
