a8d75b9e0bdb062e6c235174ce9dbd484eb03782
Jbuhrer <152345088+Jbuhrer@users.noreply.github.com>
2024-11-15 10:56:57 -0600

    Update Multiplication.py

--- Multiplication.py
+++ Multiplication.py
@@ -9,3 +9,5 @@ product = multiply_numbers(num1, num2)
 
 
 print("The product of {0} and {1} is {2}".format(num1, num2, product))
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
